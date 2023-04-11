3f0a52af8566eecf5d5f4997572d502868ada0c9
homepirate <el2409@yandex.ru>
2023-04-11 22:47:02 +0300

    add un/protected route

--- main.py
+++ main.py
@@ -66,5 +66,17 @@ def home_page(request: Request, session: AsyncSession = Depends(get_async_sessio
     )
 
 
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
+
+
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, user"
+
 
 # @app.get("/registration")
